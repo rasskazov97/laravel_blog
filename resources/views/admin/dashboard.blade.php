47dd0c23e06a8afa4d593d3e708118559dbe7f5c
Maksim <mks_2017@list.ru>
2018-10-06 10:46:36 +0500

    first_laravel

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.dashboard.admin')
+
+@section('content')
+
+<h1>Это админка</h1>
+
+@endsection
---
+++ resources/views/blog/main.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.blog.app')
+
+@section('content')
+
+<h1>Это Блог</h1>
+
+@endsection
